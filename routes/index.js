77cb8ee68bf36519bc530fa3a7502defbdc36c40
Just-AgA <161239277+Just-AgA@users.noreply.github.com>
2025-11-25 20:51:27 +0100

    Add initial list of messages to showcase at the main page

---
+++ routes/index.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const router = express.Router();
+
+const messages = [
+  {
+    text: 'Hi there!',
+    user: 'Amando',
+    added: new Date(),
+  },
+  {
+    text: 'Hello World!',
+    user: 'Charles',
+    added: new Date(),
+  },
+];
