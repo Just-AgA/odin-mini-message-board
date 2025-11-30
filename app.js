b7e107733b99687a180537129cdbda4570a79482
Just-AgA <161239277+Just-AgA@users.noreply.github.com>
2025-11-30 19:24:07 +0100

    Add engine setup

--- app.js
+++ app.js
@@ -3,3 +3,7 @@ const app = express();
 const path = require('path');
 
 const indexRouter = require('./routes/index');
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
